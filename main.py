85c361f12a7ea9b371b69e376a0a921edfe769b7
evanb5 <evanbai2004@gmail.com>
2024-09-15 23:13:42 -0400

    Face detector with reference image

---
+++ main.py
@@ -0,0 +1,50 @@
+import threading
+
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+
+match = False
+
+reference_img = cv2.imread("reference.jpg")
+
+def check_face(frame):
+    global match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            match == True
+        else:
+            match == False
+    except ValueError:
+        match == False
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+
+        if match:
+            cv2.putText(frame, "MATCH", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("video", frame)
+
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
